33b2c061883d77c492ebe6272f3a9660bc2f095e
birdhar <birat.dhar.89@gmail.com>
2024-03-17 14:17:35 +0530

    import issue of user model resolved in reset password

--- pages/api/auth/resetpass.js
+++ pages/api/auth/resetpass.js
@@ -1,5 +1,5 @@
 import { mongooseConnect } from "@/lib/mongoose";
-import { User } from "@/models/user";
+import { User } from "@/models/User";
 import Cryptr from "cryptr";
 import bcrypt from "bcryptjs";
 
